0de5ea84a7ef5585d6834a2499f290ae0ccb4b55
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-22 15:39:46 +0200

    stage 5 finished

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 DEPARTMENT_LIST = ["Biotech", "Chemistry", "Engineering", "Mathematics", "Physics"]
 
 
+department_exam = {"Biotech":1, "Chemistry":1, "Engineering":3, "Mathematics":2, "Physics":0}
+
 def main():
     # n, m = int(input()), int(input())  # int()
     # m = input()  # int()
@@ -12,10 +14,10 @@ def main():
     m = int(input())
     students = []
     departments = defaultdict(list)
-    with open('applicant_list.txt', 'r') as f:
+    with open('applicants.txt', 'r') as f:
         for line in f:
-            x, y, z, u1, u2, u3 = line.split()
-            students.append((f"{x} {y}", float(z), (u1, u2, u3)))
+            x, y, phy, chem, math, cs, u1, u2, u3 = line.split()
+            students.append((f"{x} {y}", (float(phy), float(chem), float(math), float(cs)), (u1, u2, u3)))
             # departments.setdefault(u1, []).append(student)
             # departments[u1] += 1
     # departments = sorted(departments,key=lambda x:x[0])
@@ -23,7 +25,9 @@ def main():
     #     print(s)
 
     for i in range(3):
-        students.sort(key=lambda ss: (ss[2][i],-ss[1],  ss[0]))
+        # dept = ss[2][i]
+        # exam =department_exam[dept]
+        students.sort(key=lambda ss: (ss[2][i],-ss[1][department_exam[ss[2][i]]],  ss[0]))
         # for s in students:
         #     print("*******",s)
         not_qualified = []
@@ -46,11 +50,11 @@ def main():
 
     for department in DEPARTMENT_LIST:
         student_list = departments[department]
-        student_list.sort(key=lambda stu: (-stu[1], stu[0]))
+        student_list.sort(key=lambda stu: (-stu[1][department_exam[department]], stu[0]))
         print(department)
         # selected = student_list[:m]
         for s in student_list:
-            print(f"{s[0]} {s[1]}")
+            print(f"{s[0]} {s[1][department_exam[department]]}")
 
         print()
 
