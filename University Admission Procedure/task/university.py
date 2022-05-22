4da6284475307b981abfb833e3a8dadd86b02887
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-22 12:51:10 +0200

    stage 2 finished

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,10 +1,12 @@
 # write your code here
 def main():
-
-
     scores = [int(input()) for _ in range(3)]
-    print(sum(scores)/3)
-    print("Congratulations, you are accepted!")
+    mean = sum(scores) / 3
+    print(mean)
+    if mean >= 60:
+        print("Congratulations, you are accepted!")
+    else:
+        print("We regret to inform you that we will not be able to offer you admission.")
 
 
 main()
