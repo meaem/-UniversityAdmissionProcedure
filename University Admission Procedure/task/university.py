095983cd3c2f8a11f58e2b84a4fb6a4a3b6a2cb3
Ahmed Mohamed Abdelaal <aabdelaal@gmail.com>
2022-05-22 13:18:17 +0200

    stage 3 finished

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,12 +1,26 @@
 # write your code here
 def main():
-    scores = [int(input()) for _ in range(3)]
-    mean = sum(scores) / 3
-    print(mean)
-    if mean >= 60:
-        print("Congratulations, you are accepted!")
-    else:
-        print("We regret to inform you that we will not be able to offer you admission.")
+    n, m = int(input()), int(input())  # int()
+    # m = input()  # int()
+    # print(f"*{n}*{m}*")
+    # students = [(f"{x} {y}", z) for _ in range(1) for x, y, z in input().split()]
+    students = []
+    for _ in range(n):
+        x, y, z = input().split()
+        students.append((f"{x} {y}", float(z)))
+        # print(students)
+    students.sort(key=lambda x: (-x[1], x[0]))
+    selected = students[:m]
+    print("Successful applicants:")
+    for s in selected:
+        print(s[0])
 
 
 main()
+# 5
+# 3
+# Cole Collins 3.68
+# Dolores Baldwin 3.40
+# Brett Boyer 2.45
+# Nora Alston 3.71
+# Jessy Moore 3.14
